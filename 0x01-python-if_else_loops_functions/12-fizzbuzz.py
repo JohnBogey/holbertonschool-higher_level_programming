4ae1f155002561994e710b21f2d5fbda4ffc9a03
JohnBogey <maxwellloganlovell@gmail.com>
2020-12-08 20:04:40 +0000

    turned into function

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-for n in range(1, 101):
-    if n % 3 == 0:
-        print("Fizz", end="")
-    if n % 5 == 0:
-        print("Buzz", end="")
-    if n % 3 == 0 or n % 5 == 0:
-        print(" ", end="")
-    else:
-        print("{:d} ".format(n), end="")
-print()
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 3 == 0:
+            print("Fizz", end="")
+        if n % 5 == 0:
+            print("Buzz", end="")
+        if n % 3 == 0 or n % 5 == 0:
+            print(" ", end="")
+        else:
+            print("{:d} ".format(n), end="")
+    print()
