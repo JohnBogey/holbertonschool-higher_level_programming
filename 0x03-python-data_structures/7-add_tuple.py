672e2f7326235f8a9fb2a7f5ceabc4f6f2d42d92
JohnBogey <maxwellloganlovell@gmail.com>
2020-12-15 19:19:46 +0000

    task 7, tupple add

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        list_a = [0, 0]
+    elif len(tuple_a) == 1:
+        list_a = [tuple_a[0], 0]
+    else:
+        list_a = [tuple_a[0], tuple_a[1]]
+    if len(tuple_b) == 0:
+        list_b = [0, 0]
+    elif len(tuple_b) == 1:
+        list_b = [tuple_b[0], 0]
+    else:
+        list_b = [tuple_b[0], tuple_b[1]]
+    tuple_c = list_a[0] + list_b[0], list_a[1] + list_b[1]
+    return tuple_c
+
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
