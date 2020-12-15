c0cf6165a247c353e9d35a48b0b5c078f6eb3645
JohnBogey <maxwellloganlovell@gmail.com>
2020-12-15 18:52:17 +0000

    pep8 errors

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,18 +4,8 @@ def print_matrix_integer(matrix=[[]]):
         i = 0
         for pos in row:
             if i is not 0:
-                print(" ", end = "")   
+                print(" ", end="")
             else:
                 i += 1
-            print("{:d}".format(pos), end = "")
+            print("{:d}".format(pos), end="")
         print()
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
