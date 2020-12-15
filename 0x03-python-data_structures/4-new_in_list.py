c1ec4d676d1061978e0bef78859dcd929d24fd45
JohnBogey <maxwellloganlovell@gmail.com>
2020-12-15 17:34:39 +0000

    tasks 0-4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    def new_in_list(my_list, idx, element):
+        new_list = my_list[:]
+        if idx < 0 or idx > len(new_list) - 1:
+            return new_list
+        new_list[idx] = element
+        return new_list
+        
