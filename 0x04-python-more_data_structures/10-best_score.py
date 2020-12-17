410bab9296efb1c81941554129be847d31df9580
JohnBogey <maxwellloganlovell@gmail.com>
2020-12-17 22:01:36 +0000

    code_fix not updating high score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if not a_dictionary:
         return None
-    winner = "nobody"
+    winner = ""
     best_score = 0
     for key, value in a_dictionary.items():
         if value > best_score:
+            best_score = value
             winner = key
     return winner
