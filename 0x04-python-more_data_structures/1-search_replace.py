5c1003b400d224dd54040439654f489aa97b820d
JohnBogey <maxwellloganlovell@gmail.com>
2020-12-17 20:10:23 +0000

    code_add task 1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for element in my_list:
+        new_list.append(element if element != search else replace)
+    return new_list
