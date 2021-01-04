8910c7f0ceddea1b1c26a6fb631167814c9d9346
JohnBogey <maxwellloganlovell@gmail.com>
2021-01-04 17:28:28 +0000

    code_add task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,23 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
+
     try:
         print("{:d}".format(value))
     except ValueError as ve:
-        print("Exception:", ve)
+        print("Exception:", ve, file = sys.stderr)
         return False
     return True
-
-value = 89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = -89
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
-
-value = "Holberton"
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
