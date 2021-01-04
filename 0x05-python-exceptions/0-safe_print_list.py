fd0d55934f4f3a08052825d521bd6a3b79451c37
JohnBogey <maxwellloganlovell@gmail.com>
2021-01-04 17:09:59 +0000

    fix tasks 0 and 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,9 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for i in range(x):
         try:
-            print(my_list[i])
+            print(my_list[i], end="")
         except IndexError:
-            return i
+            i -= 1
+            break
+    print("")
     return i + 1
