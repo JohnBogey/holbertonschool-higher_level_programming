202b8ea6fab6c5673ddcda7fd24e266464c59a84
JohnBogey <maxwellloganlovell@gmail.com>
2021-01-04 17:02:05 +0000

    code_add readme and tasks

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    for i in range(x):
+        try:
+            print(my_list[i])
+        except IndexError:
+            return i
+    return i + 1
