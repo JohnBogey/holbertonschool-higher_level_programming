82992546fdc23bc81be56c0cbc50bc9cb2e9cbd9
JohnBogey <maxwellloganlovell@gmail.com>
2021-01-04 17:54:53 +0000

    code_fix added an else

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -8,6 +8,4 @@ def safe_function(fct, *args):
         print("Exception: list index out of range", file=sys.stderr)
     except ZeroDivisionError:
         print("Exception: division by zero", file=sys.stderr)
-    else:
-        return None
     return None
