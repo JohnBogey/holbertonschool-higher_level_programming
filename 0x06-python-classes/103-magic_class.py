c4793c4a80fefdb24a02fb4d4392e8562c3db43f
JohnBogey <maxwellloganlovell@gmail.com>
2021-01-06 16:25:13 +0000

    code_fix extra blank lines

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 import math
-"""Comment
 
-
-"""
 class MagicClass:
-    """Magical class
-    """
     def __init__(self, radius):
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
-
 
     def area(self):
         return self.__radius ** 2 * math.pi
