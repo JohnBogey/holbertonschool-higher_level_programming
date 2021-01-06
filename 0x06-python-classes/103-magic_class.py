0f61fd8d76ee9152bd2f042aa61afa2ba9551d75
JohnBogey <maxwellloganlovell@gmail.com>
2021-01-06 16:27:09 +0000

    code_fix missing comments

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
+"""comments
+"""
+
 import math
 
+
 class MagicClass:
+    """comment"""
     def __init__(self, radius):
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
+    """comment"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+    """comment"""
         return self.__radius * math.pi * 2
