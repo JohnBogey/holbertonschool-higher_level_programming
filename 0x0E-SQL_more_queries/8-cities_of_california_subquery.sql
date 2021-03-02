3081de0d496926d1ca2b8e2ba79e6bb40832c211
JohnBogey <maxwellloganlovell@gmail.com>
2021-03-02 19:40:55 +0000

    adding task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 -- lists all cities of california
 SELECT id, name FROM cities WHERE state_id = (
-       SELECT id FROM cities WHERE name = "California"
+       SELECT id FROM states WHERE name = "California"
 ) ORDER BY id ASC;
