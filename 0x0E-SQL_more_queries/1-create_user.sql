02d260a86be5b19a9478fa2c09a31b7d1579627f
JohnBogey <maxwellloganlovell@gmail.com>
2021-03-02 18:17:43 +0000

    added dropped s

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates user with all privleges of MySql server
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
