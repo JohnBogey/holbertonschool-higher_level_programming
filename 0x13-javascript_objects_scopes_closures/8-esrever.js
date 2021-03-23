af8d64a788e9912a0cff2a52e8e60d868c5abde4
JohnBogey <maxwellloganlovell@gmail.com>
2021-03-23 18:50:32 +0000

    correcting imports and semistandard

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,9 @@
+#!/usr/bin/node
 exports.esrever = function (list) {
-  let reversed = [];
+  const reversed = [];
   for (let i = list.length; i >= 0; i--) {
     reversed.push(list[i]);
   }
   reversed.shift();
   return reversed;
-}
+};
