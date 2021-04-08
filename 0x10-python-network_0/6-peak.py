6094e376745446a19d5f9f7aa51f90761bce2c01
JohnBogey <maxwellloganlovell@gmail.com>
2021-04-08 20:49:51 +0000

    updated code to use recursion

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 """ function to find peak """
+
+
+def find_peak_recur(hill, low, high, length):
+    """ recursive function to find a peak """
+    mid = low + (high - low) / 2
+    mid = int(mid)
+    print("l[{}] c[{}] r[{}]".format(hill[mid - 1], hill[mid], hill[mid + 1]))
+    if ((mid == 0 or hill[mid - 1] <= hill[mid]) and
+       (mid == length - 1 or hill[mid + 1] <= hill[mid])):
+        return hill[mid]
+    elif (mid < length and hill[mid + 1] < hill[mid]):
+        return find_peak_recur(hill, low, (mid - 1), length)
+    else:
+        return find_peak_recur(hill, (mid + 1), high, length)
+
+
 def find_peak(list_of_integers):
-    if not list_of_integers or len(list_of_integers) < 1:
+    """ wrapper function to run recur func """
+    if not list_of_integers:
         return None
-    hill = list_of_integers
-    for i in range(len(hill)):
-        if hill[i - 1] < hill[i] and hill[i + 1] < hill[i]:
-            return hill[i]
-    return hill[len(hill) - 1]
+    length = len(list_of_integers)
+    return find_peak_recur(list_of_integers, 0, length - 1, length)
