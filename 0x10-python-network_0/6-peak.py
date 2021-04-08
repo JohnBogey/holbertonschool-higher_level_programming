b95a93209e42e1a5fe728585c2661e3b1efa0e18
JohnBogey <maxwellloganlovell@gmail.com>
2021-04-08 20:50:27 +0000

    removed print

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,7 +6,6 @@ def find_peak_recur(hill, low, high, length):
     """ recursive function to find a peak """
     mid = low + (high - low) / 2
     mid = int(mid)
-    print("l[{}] c[{}] r[{}]".format(hill[mid - 1], hill[mid], hill[mid + 1]))
     if ((mid == 0 or hill[mid - 1] <= hill[mid]) and
        (mid == length - 1 or hill[mid + 1] <= hill[mid])):
         return hill[mid]
