5a7a6ba51153b4ebf6247e94e85184c871583451
JohnBogey <maxwellloganlovell@gmail.com>
2021-04-09 18:36:25 +0000

    moved imports

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 ''' takes in URL displays response or error '''
-import requests
-from sys import argv
 
 if __name__ == "__main__":
     ''' main code '''
+    import requests
+    from sys import argv
     r = requests.get(argv[1])
     print(r.headers['X-Request-Id'])
