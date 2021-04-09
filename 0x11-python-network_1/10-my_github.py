3cb25e152259bdf819e9047a32b3264ffb4953fd
JohnBogey <maxwellloganlovell@gmail.com>
2021-04-09 20:51:26 +0000

    my github id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' takes in URL displays response or error '''
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    ''' main code '''
+    url = "https://api.github.com/user"
+    try:
+        print(requests.get(url, auth=(argv[1], argv[2])).json()['id'])
+    except:
+        print("None")
