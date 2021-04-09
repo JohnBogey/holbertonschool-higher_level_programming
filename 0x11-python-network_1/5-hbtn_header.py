0ee44a2c56789dc10db4dbb8c7cf2aeeaf35b4d0
JohnBogey <maxwellloganlovell@gmail.com>
2021-04-09 18:28:57 +0000

    swapped placeholder to argv

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,5 +5,5 @@ from sys import argv
 
 if __name__ == "__main__":
     ''' main code '''
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get(argv[1])
     print(r.headers["x-request-id"])
