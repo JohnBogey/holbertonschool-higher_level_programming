1dd1b384b04d0ff40613872bb11bddebfc678581
JohnBogey <maxwellloganlovell@gmail.com>
2021-05-04 13:23:58 -0400

    error handling fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,8 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response) {
+  if (error) {
+    console.error(error); // Print the error if one occurred
+  }
   console.log('code:', response && response.statusCode);
 });
