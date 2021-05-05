23b3be2881610884ead4c4b57ed95441e0c2f165
JohnBogey <maxwellloganlovell@gmail.com>
2021-05-05 10:35:52 -0400

    advanced tasks

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,18 +2,19 @@
 // Print characters name
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/';
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
 request(url, function (error, response, body) {
   if (error) {
     console.error(error); // Print the error if one occurred
   } else {
-    for (const person of JSON.parse(body).results) {
-      for (const film of person.films) {
-        if (film.split('/')[5] === process.argv[2]) {
-          console.log(person.name);
-          break;
+    for (const person of JSON.parse(body).characters) {
+      request(person, function (error2, response2, body2) {
+        if (error2) {
+          console.error(error2); // Print the error if one occurred
+        } else {
+          console.log(JSON.parse(body2).name);
         }
-      }
+      });
     }
   }
 });
---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Print characters name
+
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error); // Print the error if one occurred
+  } else {
+    for (const person of JSON.parse(body).characters) {
+      request(person, function (error2, response2, body2) {
+        if (error2) {
+          console.error(error2); // Print the error if one occurred
+        } else {
+          console.log(JSON.parse(body2).name);
+        }
+      });
+    }
+  }
+});
