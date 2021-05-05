145dcc193b30c822e70927b8af63f3a2816ec56a
JohnBogey <maxwellloganlovell@gmail.com>
2021-05-05 11:03:31 -0400

    async

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,17 +3,20 @@
 
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
-request(url, function (error, response, body) {
+request(url, async function (error, response, body) {
   if (error) {
     console.error(error); // Print the error if one occurred
   } else {
     for (const person of JSON.parse(body).characters) {
-      request(person, function (error2, response2, body2) {
-        if (error2) {
-          console.error(error2); // Print the error if one occurred
-        } else {
-          console.log(JSON.parse(body2).name);
-        }
+      await new Promise( function (resolve, reject) {
+        request(person, function (error2, response2, body2) {
+          if (error2) {
+            console.error(error2); // Print the error if one occurred
+          } else {
+            console.log(JSON.parse(body2).name);
+          }
+          resolve();
+        });
       });
     }
   }
